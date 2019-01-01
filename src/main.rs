e1c5443a4d306b6c6357a560d564a44187621089
Enrico Stano <ocirne.onats@gmail.com>
2019-01-01 20:28:36 +0100

    :boom::boom:

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 extern crate clap;
+extern crate hubcaps;
 extern crate reqwest;
 extern crate select;
-#[macro_use]
-extern crate serde_json;
+extern crate tokio;
 
-use clap::{App};
+use clap::App;
+use hubcaps::{Credentials, Github, git::GetReferenceResponse::Exact, git::GetReferenceResponse::StartWith};
 use select::document::Document;
 use select::predicate::Name;
-use serde_json::Value;
+use tokio::runtime::Runtime;
 
 fn document_from(url: &str) -> Document {
     let client = reqwest::Client::new();
@@ -27,39 +28,62 @@ fn volantino_code_from(url: &str) -> String {
 }
 
 fn main() {
-//    let matches = App::new("volantino")
-//        .version("0.1")
-//        .get_matches();
+    //    let matches = App::new("volantino")
+    //        .version("0.1")
+    //        .get_matches();
 
     let buffetti_url = "https://buffetti.it/shopping-bag/";
     let stanosas_url = "http://www.stanosas.it";
     let buffetti_volantino_code = volantino_code_from(buffetti_url);
 
-    if buffetti_volantino_code == volantino_code_from(stanosas_url) {
+    if buffetti_volantino_code != volantino_code_from(stanosas_url) {
         println!("Both sites use code: {:?}", buffetti_volantino_code)
     } else {
-        println!("Buffetti released a new code: {:?}", buffetti_volantino_code);
+        println!(
+            "Buffetti released a new code: {:?}",
+            buffetti_volantino_code
+        );
 
-        //let api_token = "254c827df7507efd78d96d2fa98392e24e47962c";
-        //let client = Github::new(api_token).unwrap();
-        // 1. create a new branch from master
+        let github = Github::new(
+            "my-cool-user-agent/0.1.0",
+            Credentials::Token(api_token.to_string()),
+        );
+        let mut rt = Runtime::new().unwrap();
+
+        // 1. create a new branch from gh-pages
+        // 1.a. get gh-pages branch's SHA
+        match rt.block_on(
+            github
+                .repo("enricostano", "stanosas")
+                .git()
+                .reference("heads/gh-pages")
+        ) {
+            Ok(reference) => {
+                match reference {
+                    Exact(reference) => { let current_branch_sha = reference.object.sha; },
+                    StartWith(_) => { panic!("There are many branches starting with heads/gh-pages") }
+                }
+                println!("SHA {:#?}", current_branch_sha);
+            },
+            Err(err) => println!("err {:#?}", err),
+        }
 
         // 2. create a new content updating the file with the new code
         //    https://developer.github.com/v3/repos/contents/
         // 3. create a new PR
-//        let body = json!(
-//            {
-//                "title": "hola",
-//                "body": "The code for the Volantino is changed. The new code is"
-//            }
-//            );
-//        let me = client.get()
-//            .repos()
-//            .owner("enricostano")
-//            .repo("stanosas")
-//            .git()
-//            .refs()
-//            .execute::<Value>();
-//        println!("code: {:?}", me)
+        //        let body = json!(
+        //            {
+        //                "title": "hola",
+        //                "body": "The code for the Volantino is changed. The new code is"
+        //            }
+        //            );
+        //        let me = client.get()
+        //            .repos()
+        //            .owner("enricostano")
+        //            .repo("stanosas")
+        //            .git()
+        //            .refs()
+        //            .execute::<Value>();
+        //        println!("code: {:?}", me)
     }
 }
